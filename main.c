b7bf10c0c8bd633fb1b99569eec284917a042104
FredVenturin <frederico.venturin@gmail.com>
2025-08-05 22:16:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,19 +8,20 @@ int main(void)
   float A,B,C;
   printf ("Calculadora de Hipotenusa (A): Catetos (B e C).\n");
 
-  printf("Valor de B: \n");
+  printf("Valor de B: ");
   scanf("%f",&B);
 
-  printf("Valor de C: \n");
+  printf("Valor de C: ");
   scanf("%f",&C);
 
   float B1 = pow(B,2);
   float C1 = pow(C,2);
 
-  A = pow(B1 + C1, 1.0/2);
+  A = sqrt(B1 + C1);
 
   printf("A hipotenusa 'A' mede %.1f \n", A );
 
   return 0;
 
 }
+
